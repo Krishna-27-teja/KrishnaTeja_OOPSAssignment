7ef7335490a021978561b0267902f64020f1c001
Krishna-27-teja <76254392+Krishna-27-teja@users.noreply.github.com>
2022-03-27 21:44:26 +0530

    Add files via upload

---
+++ SuperDepartment.java
@@ -0,0 +1,21 @@
+package com.greatlearning.main;
+
+public class SuperDepartment {
+	public String departmentName()
+	{
+		return "super Department";
+	}
+	public String getTodaysWork()
+	{
+		return "No work as of now";
+	}
+	public String getWorkDeadline()
+	{
+		return "Nil";
+	}
+	public String isTodayAHoliday()
+	{
+		return "Today is not a Holiday";
+	}
+
+}
